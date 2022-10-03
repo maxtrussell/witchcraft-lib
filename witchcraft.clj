af8b4d19dc783c5a68b4ffca6f0f5af7abaa42fa
Maxwell Trussell <maxtrussell@gmail.com>
2022-10-02 21:05:26 -0700

    Add append and set-key macros

--- witchcraft.clj
+++ witchcraft.clj
@@ -3,25 +3,31 @@
 
 (use 'clojure.string)
 
+(defmacro append [xs x]
+  `(def ~xs (conj ~xs ~x)))
+
+(defmacro set-key [m k v]
+  `(def ~m (assoc ~m ~k ~v)))
+
 (defn get-inv-contents [player]
   (def contents (hash-map))
   (let [inv (remove nil? (get (wc/inventory player) :contents))]
     (doseq [item inv]
       (let [material (get item :material)]
-            (def contents (assoc contents material (+ (get contents material 0) (get item :amount))))))
+        (set-key contents material (+ (get contents material 0) (get item :amount)))))
     contents))
 
 (defn player-has-materials? [player materials]
   (def remaining (get-inv-contents player))
   (doseq [[material qty] materials]
     (when (not (nil? material))
-      (def remaining (assoc remaining material (- (get remaining material 0) qty)))))
+      (set-key remaining material (- (get remaining material 0) qty))))
   (reduce (fn [acc [material qty]] (and acc (>= qty 0))) true remaining))
 
 (defn get-materials [materials]
   (def total (hash-map))
   (doseq [item materials]
-    (def total (assoc total (nth item 3) (+ (get total (nth item 3) 0) 1))))
+    (set-key total (nth item 3) (+ (get total (nth item 3) 0) 1)))
   total)
 
 (defn build [player pos blocks]
@@ -47,23 +53,24 @@
   (def sum-map (hash-map))
   (doseq [m maps]
     (doseq [[k v] m]
-      (def sum-map (assoc sum-map k (+ (get sum-map k 0) v)))))
+      (set-key sum-map k (+ (get sum-map k 0) v))))
   sum-map)
 
 (defn dfs [loc pred? func!]
   "Performs a depth first search of all blocks matching pred? and applies func. Returns map of found blocks."
   (def found (hash-map))
+  ;; FIXME: Seen isn't properly being used by recursive calls
   (def seen (set '()))
   (let* [block (wc/block loc)
          material (get block :material)]
     (if (pred? block)
-      (do (def found (assoc found material (+ (get found material 0) 1)))
+      (do (set-key found material (+ (get found material 0) 1))
           (func! block)
           (doseq [n (get-neighbors loc)]
             (when (nil? (seen n))
               (def found (add-maps [found (dfs n pred? func!)]))
-              (def seen (conj seen n)))))))
-  found))
+              (append seen n))))))
+  found)
 
 (defn get-neighbors [loc]
   "Find all neighbors of a given block"
@@ -92,8 +99,7 @@
           :when (or (is-wall? x dx) (is-wall? z dz) (is-roof? y dy))]
       [x y z material])))
 
-
-(fell-tree me {:x -7 :y 72 :z -54})
+(fell-tree me {:x -6 :y 76 :z -52})
 (wc/undo!)
 
 (def me (wc/player "maximus1233"))
