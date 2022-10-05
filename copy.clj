ef78556677cf8991198563ee6bc329c77457c062
CoyTorreblanca <coydiego@knights.ucf.edu>
2022-10-05 08:54:42 -0500

    Use copy stick to copy. Fix clipboard is nil error. and more...

--- copy.clj
+++ copy.clj
@@ -2,8 +2,6 @@
     (:require [lambdaisland.witchcraft :as wc]
               [lambdaisland.witchcraft.events :as e]))
 
-
-
 (defn apply-fun-to-cords [fun blockv cord]
   "apply fun to cord coordinate and blokv coordinate"
   (reduce #(assoc %1 %2 (fun (get blockv %2) (get cord %2))) blockv [0 1 2])
@@ -39,12 +37,8 @@
         (if (< cord-axis axis-min)
           (def output (assoc output axis (assoc (get output axis) 0 cord-axis))))
         (if (> cord-axis axis-max)
-          (def output (assoc output axis (assoc (get output axis) 1 cord-axis))))
-        )
-      )
-   )
-  output
- )
+          (def output (assoc output axis (assoc (get output axis) 1 cord-axis)))))))
+  output)
 
 (defn min-max-range [min-max-list]
   "Create a list using inclusive range given a list of minimums and maximus."
@@ -58,60 +52,73 @@
 
 (defn copy [cords]
   "Copy blocks at cord"
-  (let [ blocks (map #(wc/blockv %) cords) ]
-    (let [output (map #(apply-fun-to-cords - % (get @ls 0)) blocks)]
-      (prn output)
-      (reset! clipboard output)
-      )
-    ))
+  (swap! clipboard  (fn [_] (mapv #(apply-fun-to-cords - % (get @ls 0)) (map #(wc/blockv %) cords)))))
 
-(defn listen-for-fun [fun]
+(defn create-copy-stick []
+  (let [stick (wc/item-stack :stick)]
+    (wc/set-display-name stick "Copy Stick")
+    (wc/set-lore stick ["Use to copy blocks."])
+    stick))
+
+(defn give-copy-stick-to-player [player]
+  (wc/add-inventory player (create-copy-stick)))
+
+(defn listen-for-fun [fun target-player]
   "Set up listener and pass cords passed by player to fun"
   (e/listen!
    :player-interact
    ::copy-setup
    (fn [{:keys [clickedBlock player action]}]
-     (let [location (wc/xyz clickedBlock) ls-count (count @ls)]
-       (when (< ls-count 2)
-         (reset! ls (conj @ls location)) ;; add cords of player interacted block to ls
-          (when (= (count @ls) 2) ;; on two blocks get all cords, pass to fun, reset ls
-            (let [ cords
-              (transform-cord-axis-lists
-                (min-max-range
-                  (get-min-max-cords-list @ls)))] ;; cords is [[xyz]]
-              (fun cords))
-            (reset! ls [])
-          )
-            )))))
+     (when (and
+            (= target-player player)
+            (not (nil? clickedBlock))
+            (= (wc/display-name (wc/item-in-hand player)) "Copy Stick"))
+        (let [location (wc/xyz clickedBlock) ls-count (count @ls)]
+        (when (< ls-count 2)
+            (reset! ls (conj @ls location)) ;; add cords of player interacted block to ls
+            (when (= (count @ls) 2) ;; on two blocks get all cords, pass to fun, reset ls
+                (let [ cords
+                (transform-cord-axis-lists
+                    (min-max-range
+                    (get-min-max-cords-list @ls)))] ;; cords is [[xyz]]
+                (fun cords))
+                (reset! ls [])
+                (wc/unlisten!
+                :player-interact
+                ::copy-setup))))))))
 
-(defn listen-for-copy []
-  (def ls (atom []))
-  (def clipboard (atom []))
-  (listen-for-fun copy)
-  )
-
-(defn unlisten-copy []
-  (wc/unlisten!
-   :player-interact
-   ::copy-setup))
-
-
-
-
-(defn paste []
+(defn paste [target-player pred?]
   (e/listen!
    :player-interact
    ::paste-setup
    (fn [{:keys [clickedBlock player action]}]
-     (let [blocks (map #(apply-fun-to-cords + % (wc/xyz clickedBlock)) @clipboard)]
-       (wc/set-blocks blocks)
-       ))))
+     (when (and
+            (= target-player player)
+            (not (nil? clickedBlock))
+            (= (wc/display-name (wc/item-in-hand player)) "Copy Stick")
+            (not (= @clipboard [])))
+        (let [blocks (map #(apply-fun-to-cords + % (wc/xyz clickedBlock)) (filter pred? @clipboard))]
+          (wc/set-blocks blocks)
+          (wc/unlisten! :player-interact ::paste-setup)
+          )))))
 
+(defn paste-without-air [player]
+  (paste player (fn [block] (not (= (wc/material block) (wc/material :air))))))
 
-;; Testing functions
-(listen-for-copy)
-(prn @clipboard)
-(wc/unlisten! :player-interact ::copy-setup)
-(wc/unlisten! :player-interact ::paste-setup)
-(wc/undo!)
-(paste)
+(defn listen-for-copy [player]
+  (def ls (atom []))
+  (def clipboard (atom []))
+  (listen-for-fun copy player))
+
+;; Testing function
+(comment
+  (give-copy-stick-to-player (wc/player "coytorreblanca"))
+  (listen-for-copy (wc/player "coytorreblanca"))
+  (wc/unlisten! :player-interact ::copy-setup)
+  (wc/unlisten! :player-interact ::paste-setup)
+  (wc/undo!)
+  (prn @clipboard)
+
+  (paste-without-air (wc/player "coytorreblanca"))
+
+  (wc/fly! (wc/player)))
